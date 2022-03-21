23a3e57c8f99b0ee6ac6e835dd3eb09c84cd4b00
elem <herbet.hadrien@gmail.com>
2022-03-21 14:56:23 +0100

    [FIX] fix the models

--- main.py
+++ main.py
@@ -3,8 +3,22 @@ from itertools import chain, combinations
 import time
 
 
-from models.action import ActionObj as m_action
+class ActionModel():
+    """Object who stock the data about each action"""
 
+    def __init__(self, name, cost, price, profit):
+        """ This object will stock all the data for an action
+        Args:
+            name (str): the name of the action
+            cost (int): the cost of the action
+            price (int): the value in the market of this action
+            profit (int): the profit after 2 year
+        """
+
+        self.name = name
+        self.cost = cost
+        self.price = price
+        self.profit = profit
 
 class ControllerRunProgram:
     """Object controller for the all program
@@ -37,7 +51,7 @@ class ControllerRunProgram:
         print("max: ", self.proposal["total_gain"])
 
     def set_action_object(self):
-        list_action = [self.list_action.append(m_action(data[0], data[1], data[2], round(
+        list_action = [self.list_action.append(ActionModel(data[0], data[1], data[2], round(
             data[1] * (data[2] + 1), 2))) for data in self.file_excel]
 
     def generate_combinason(self):
