7a0fb4b54dd8466fbdee36e5c64012bd445d36da
elem <herbet.hadrien@gmail.com>
2022-03-30 14:04:44 +0200

    [IMP] check if price is > 0

--- opti.py
+++ opti.py
@@ -1,3 +1,4 @@
+import pdb
 import pandas as pd
 import time
 
@@ -15,7 +16,9 @@ class ActionModel():
 class ControllerRunProgram:
 
     def __init__(self, budget=500):
-        self.file_excel = pd.read_excel('annexe/action.xlsx')
+        # self.file_excel = pd.read_excel('annexe/action.xlsx')
+        # self.file_excel = pd.read_excel('annexe/dataset1_Python+P7.xlsx')
+        self.file_excel = pd.read_excel('annexe/dataset2_Python+P7.xlsx')
         self.file_excel = self.file_excel.to_numpy()
 
         self.list_action = []
@@ -37,6 +40,7 @@ class ControllerRunProgram:
         self.proposal["total_gain"] = round(self.proposal["total_gain"], 2)
         print("combi", end - start)
         print("max: ", self.proposal["total_gain"])
+        import pdb; pdb.set_trace()
 
     def set_action_object(self):
         self.list_action = [ActionModel(data[0], data[1], data[2], round(
@@ -47,7 +51,7 @@ class ControllerRunProgram:
         [self.create_combi(action) for action in self.list_action]
 
     def create_combi(self, action):
-        if self.budget >= action.cost:
+        if self.budget >= action.cost and action.cost > 0:
             self.proposal["total_gain"] += action.profit
             self.proposal["budget"] += action.cost
             self.proposal["benef"] += action.benef
